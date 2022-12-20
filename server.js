51df19461c0bb7582b3aca802e063ff957ad234e
George Karamanis <gkaramanis1@outlook.com>
2022-12-20 18:51:01 -0500

    moved files

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
