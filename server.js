9469206667756c7139434ac652b252689f0e9c09
George Karamanis <gkaramanis1@outlook.com>
2022-12-20 19:02:06 -0500

    moved files

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
@@ -31,7 +31,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.post('/api/notes', (req, res) => {
